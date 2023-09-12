83c196aabca4ddcee0d2dc2dfc3e34ffb9ee4293
aditya tadas <141910946+adityatadas24@users.noreply.github.com>
2023-09-12 14:34:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 // Your Script here.
+function rot13(str) {
+  // Define a function to decode a single character
+  function decodeChar(char) {
+    // Check if the character is a letter
+    if (/[A-Za-z]/.test(char)) {
+      // Determine the character code offset based on case
+      const offset = char >= 'a' ? 97 : 65;
+      // Apply the ROT13 shift (13 positions forward or backward)
+      return String.fromCharCode(((char.charCodeAt(0) - offset + 13) % 26) + offset);
+    }
+    // If the character is not a letter, return it unchanged
+    return char;
+  }
+
+  // Use the decodeChar function to decode each character in the input string
+  const decodedChars = str.split('').map(decodeChar);
+  // Join the decoded characters back into a string
+  const decodedStr = decodedChars.join('');
+  return decodedStr;
+}
+
+// Example usage:
+const encodedString = 'Gur dhvpx oebja sbk whzcf bire gur ynml qbt.';
+const decodedString = rot13(encodedString);
+console.log(decodedString); // Output: 'The quick brown fox jumps over the lazy dog.'
+
 
 const lookup = {
   A: "N",
